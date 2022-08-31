9c708b9dea593681229e2e0ad9f6fde7248cf45e
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2022-08-31 11:37:53 +0300

    Add user module

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,31 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import { Exclude } from 'class-transformer';
+import { IsEmail, Length } from 'class-validator';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  @Exclude()
+  @Length(8, 20)
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  async comparePassword(attempt: string) {
+    return await bcrypt.compare(attempt, this.password);
+  }
+}
